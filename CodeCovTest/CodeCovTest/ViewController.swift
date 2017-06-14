842d7b51a6072e130769677d78db1800a7b40d1d
Srikanth <smadhusudhan@mast.com>
2017-06-14 21:20:43 +0530

    Initial commit

---
+++ CodeCovTest/CodeCovTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CodeCovTest
+//
+//  Created by Madhusudhan, Srikanth on 6/14/17.
+//  Copyright © 2017 GoodSp33d. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
